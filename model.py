6d6c6b4d63651509648fddf9ddc2561537d477da
Sarah Smruthi <smruthisarah@gmail.com>
2025-01-18 20:34:01 +0530

    updated model.py

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 # Import required libraries
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from sklearn.model_selection import train_test_split, GridSearchCV 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression, SGDClassifier
